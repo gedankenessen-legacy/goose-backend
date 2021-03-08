f15d503524a554286d6791e84e6d393a8fb353ff
Lukas Tartler <ThrowawayAccount1@web.de>
2021-03-08 17:21:11 +0100

    added required infrastructure for the project endpoint

---
+++ Goose.API/Repositories/ProjectRepository.cs
@@ -0,0 +1,23 @@
+﻿using Goose.Data.Context;
+using Goose.Data.Repository;
+using Goose.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goose.API.Repositories
+{
+    public interface IProjectRepository: IRepository<Project>
+    {
+
+    }
+
+    public class ProjectRepository : Repository<Project>, IProjectRepository
+    {
+        public ProjectRepository(IDbContext context) : base(context, "projects")
+        {
+
+        }
+    }
+}
---
+++ Goose.Domain/Models/Project.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Goose.Data.Models;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Goose.Domain.Models
+{
+    public class Project : Document
+    {
+        public ProjectUsers[] Users { get; set; }
+        public ProjectDetails Details { get; set; }
+        public State[] States { get; set; }
+    }
+
+    public class ProjectUsers : Document
+    {
+        [BsonElement("user_id")]
+        public string UserID { get; set; }
+        [BsonElement("role_ids")]
+        public string[] RoleIDs { get; set; }
+    }
+
+    public class ProjectDetails
+    {
+        public string Name { get; set; }
+    }
+
+    public class State : Document
+    {
+        // These are the only valid values for Phase
+        const string NegotiationPhase = "Negotiation";
+        const string InProcessPhase = "InProcess";
+        const string ConclusionPhase = "Conclusion";
+
+        public string Name { get; set; }
+        public string Phase { get; set; }
+        public bool UserGenerated { get; set; }
+    }
+}
