38e4d541f51624f501fee7f27bd3f9007c193010
Lukas Tartler <ThrowawayAccount1@web.de>
2021-03-08 17:21:11 +0100

    added required infrastructure for the project endpoint

---
+++ Goose.API/Controllers/ProjectController.cs
@@ -0,0 +1,53 @@
+﻿using Goose.API.Services;
+using Goose.Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goose.API.Controllers
+{
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        // POST: api/project
+        [HttpPost]
+        public async Task<ActionResult<ProjectDTO>> CreateProject([FromBody] ProjectDTO projectDTO)
+        {
+            var newCompany = await _projectService.CreateNewProjectAsync(projectDTO);
+            return Ok(newCompany); 
+        }
+
+        // PUT: api/project/{id}
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ProjectDTO>> UpdateProject(string id, [FromBody] ProjectDTO projectDTO)
+        {
+            var updatedProject = await _projectService.UpdateProject(id, projectDTO);
+            return Ok(updatedProject);
+        }
+
+        // GET: api/project
+        [HttpGet]
+        public async Task<ActionResult<ProjectDTO[]>> GetProjects()
+        {
+            var projectIter = _projectService.GetProjects();
+            var projects = await projectIter.ToArrayAsync();
+            return Ok(projects);
+        }
+
+        // GET: api/project/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProjectDTO>> GetProjects(string id)
+        {
+            var projects = await _projectService.GetProject(id);
+            return Ok(projects);
+        }
+    }
+}
---
+++ Goose.API/Services/ProjectService.cs
@@ -0,0 +1,48 @@
+﻿using Goose.API.Repositories;
+using Goose.Domain.DTOs;
+using Goose.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goose.API.Services
+{
+    public interface IProjectService
+    {
+        Task<ProjectDTO> CreateNewProjectAsync(ProjectDTO requestedProject);
+        Task<ProjectDTO> UpdateProject(string id, ProjectDTO projectDTO);
+        IAsyncEnumerable<ProjectDTO> GetProjects();
+        Task<ProjectDTO> GetProject(string id);
+    }
+
+    public class ProjectService : IProjectService
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectService(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public Task<ProjectDTO> CreateNewProjectAsync(ProjectDTO requestedProject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectDTO> GetProject(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<ProjectDTO> GetProjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectDTO> UpdateProject(string id, ProjectDTO projectDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
