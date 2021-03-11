0647246c5cc17fe7b6b60b416213a5d75f0eeb05
Willy <Willy@DESKTOP-KJ7KA7P>
2021-03-11 16:20:55 +0100

    Änderungen beinhalten: + Repository hinzugefügt + Service hinzugefügt + Controller hinzugefügt Man kann Roles erstellen, updaten und holen.

---
+++ Goose.API/Controllers/RoleController.cs
@@ -0,0 +1,66 @@
+﻿using Goose.API.Services;
+using Goose.Domain.Models.identity;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goose.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : Controller
+    {
+        private readonly IRoleService _roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Role>>> GetRolesAsync()
+        {
+            var roles = await _roleService.GetRolesAsync();
+            return Ok(roles);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Role>> GetRolesAsync(string id)
+        {
+            ObjectId objectId = new ObjectId(id);
+            var role = await _roleService.GetRoleAsync(objectId);
+            return Ok(role);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Role>> CreateRoleAsync([FromBody] Role role)
+        {
+            try
+            {
+                var newRole = await _roleService.CreateRoleAsync(role);
+                return Ok(newRole);
+            } 
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Role>> UpdateRoleAsync([FromBody] Role role)
+        {
+            try
+            {
+                var roleToUpdate = await _roleService.UpdateRoleAsync(role);
+                return Ok(roleToUpdate);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
---
+++ Goose.API/Services/RoleService.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Goose.API.Repositories;
+using Goose.Domain.Models.identity;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goose.API.Services
+{
+    public interface IRoleService
+    {
+        Task<Role> GetRoleAsync(ObjectId id);
+        Task<IList<Role>> GetRolesAsync();
+        Task<Role> CreateRoleAsync(Role role);
+        Task<Role> UpdateRoleAsync(Role role);
+    }
+
+    public class RoleService : IRoleService
+    {
+        private readonly IRoleRepository _roleRepository;
+        private readonly IMapper _mapper;
+
+        public RoleService(IRoleRepository roleRepository, IMapper mapper)
+        {
+            _roleRepository = roleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Role> GetRoleAsync(ObjectId id)
+        {
+            var role = await _roleRepository.GetAsync(id);
+
+            if (role == null)
+                throw new Exception();
+
+            return role;
+        }
+
+        public async Task<IList<Role>> GetRolesAsync()
+        {
+            return await _roleRepository.GetAsync();
+        }
+
+        public async Task<Role> CreateRoleAsync(Role role)
+        {
+            if (role == null)
+                throw new Exception();
+
+            if (string.IsNullOrWhiteSpace(role.Name))
+                throw new Exception();
+
+            var newRole = new Role()
+            {
+                Name = role.Name
+            };
+
+            await _roleRepository.CreateAsync(newRole);
+
+            return newRole;
+        }
+
+        public async Task<Role> UpdateRoleAsync(Role role)
+        {
+            if (role == null)
+                throw new Exception();
+
+            if (string.IsNullOrWhiteSpace(role.Name))
+                throw new Exception();
+
+            var roleToUpdate = await _roleRepository.GetAsync(role.Id);
+
+            if (roleToUpdate == null)
+                throw new Exception();
+
+            roleToUpdate.Name = role.Name;
+
+            await _roleRepository.UpdateAsync(roleToUpdate);
+
+            return roleToUpdate;
+        }
+    }
+}
