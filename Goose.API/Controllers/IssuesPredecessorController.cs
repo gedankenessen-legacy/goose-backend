21ffd9703332e376a5f905073aa8e5df973aade0
Yochyo <herodragon25@gmail.com>
2021-03-16 12:28:15 +0100

    Nachfolger/Vorgänger können jetzt hinzugefügt/entfernt werden

---
+++ Goose.API/Controllers/IssuesPredecessorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Goose.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Goose.API.Controllers
+{
+    [Route("api/projects/{projectId}/issues/{issueId}/predecessors/")]
+    [ApiController]
+    public class IssuesPredecessorController : Controller
+    {
+        private readonly IIssuePredecessorService _issueService;
+
+        public IssuesPredecessorController(IIssuePredecessorService issueService)
+        {
+            _issueService = issueService;
+        }
+
+        [HttpPut("{predecessorId}")]
+        public async Task<ActionResult> SetPredecessor([FromRoute] string projectId,
+            [FromRoute] string issueId, [FromBody] string predecessorId)
+        {
+            await _issueService.SetPredecessor(projectId.TryParse(), issueId.TryParse(),
+                predecessorId.TryParse());
+            return NoContent();
+        }
+
+        [HttpDelete("{predecessorId}")]
+        public async Task<ActionResult> RemovePredecessor([FromRoute] string projectId,
+            [FromRoute] string issueId, [FromBody] string predecessorId)
+        {
+            await _issueService.RemovePredecessor(projectId.TryParse(), issueId.TryParse(),
+                predecessorId.TryParse());
+            return NoContent();
+        }
+    }
+}
---
+++ Goose.API/Services/IssuesPredecessorService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Goose.API.Repositories;
+using MongoDB.Bson;
+
+namespace Goose.API.Services
+{
+    public interface IIssuePredecessorService
+    {
+        public Task SetPredecessor(ObjectId projectId, ObjectId successorId, ObjectId predecessorId);
+        public Task RemovePredecessor(ObjectId projectId, ObjectId successorId, ObjectId predecessorId);
+    }
+
+    public class IssuesPredecessorService : IIssuePredecessorService
+    {
+        //TODO ggf müssen vorgänger rekursiv entfernt werden?
+        //TODO checken ob es eine dealock gäbe
+        //TODO wie bekommt man am besten alle issues in einem vorgänger baum? phil fragen?
+        private readonly IIssueRepository _issueRepo;
+
+        public IssuesPredecessorService(IIssueRepository issueRepo)
+        {
+            _issueRepo = issueRepo;
+        }
+
+        public async Task SetPredecessor(ObjectId projectId, ObjectId successorId, ObjectId predecessorId)
+        {
+            var successor = await _issueRepo.GetOfProjectAsync(projectId, successorId);
+            var predecessor = await _issueRepo.GetOfProjectAsync(projectId, predecessorId);
+            successor.PredecessorIssueIds.Add(predecessorId);
+            predecessor.SuccessorIssueIds.Add(successorId);
+
+            await Task.WhenAll(_issueRepo.UpdateAsync(successor), _issueRepo.UpdateAsync(predecessor));
+        }
+
+        public async Task RemovePredecessor(ObjectId projectId, ObjectId successorId, ObjectId predecessorId)
+        {
+            var successor = await _issueRepo.GetOfProjectAsync(projectId, successorId);
+            var predecessor = await _issueRepo.GetOfProjectAsync(projectId, predecessorId);
+            successor.PredecessorIssueIds.Remove(predecessorId);
+            predecessor.SuccessorIssueIds.Remove(successorId);
+
+            await Task.WhenAll(_issueRepo.UpdateAsync(successor), _issueRepo.UpdateAsync(predecessor));
+        }
+    }
+}
