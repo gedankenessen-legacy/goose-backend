59fea7f9509b6770057d25b6a85865897dfedc52
Yochyo <herodragon25@gmail.com>
2021-03-16 21:42:53 +0100

    IssueAssignedUsersController.cs hinzugefügt

---
+++ Goose.API/Controllers/IssueAssignedUsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Goose.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Goose.API.Controllers
+{
+    [Route("api/issues/{issueId}/users/")]
+    [ApiController]
+    public class IssueAssignedUsersController : Controller
+    {
+        private readonly IIssueAssignedUserService _issueService;
+
+        public IssueAssignedUsersController(IIssueAssignedUserService issueService)
+        {
+            _issueService = issueService;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put([FromRoute] string issueId, [FromRoute] string id)
+        {
+            await _issueService.AssignUser(issueId.ToObjectId(), id.ToObjectId());
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] string issueId, [FromRoute] string id)
+        {
+            await _issueService.UnassignUser(issueId.ToObjectId(), id.ToObjectId());
+            return NoContent();
+        }
+    }
+}
---
+++ Goose.API/Services/IssueAssignedUserService.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Goose.API.Repositories;
+using MongoDB.Bson;
+
+namespace Goose.API.Services
+{
+    public interface IIssueAssignedUserService
+    {
+        Task AssignUser(ObjectId issueId, ObjectId userId);
+        Task UnassignUser(ObjectId issueId, ObjectId userId);
+    }
+
+    public class IssueAssignedUserService: IIssueAssignedUserService
+    {
+        private readonly IIssueRepository _issueRepo;
+
+        public IssueAssignedUserService(IIssueRepository issueRepo)
+        {
+            _issueRepo = issueRepo;
+        }
+
+        public async Task AssignUser(ObjectId issueId, ObjectId userId)
+        {
+            var issue = await _issueRepo.GetAsync(issueId);
+            if (!issue.AssignedUserIds.Contains(userId))
+                issue.AssignedUserIds.Add(userId);
+            await _issueRepo.UpdateAsync(issue);
+        }
+
+        public async Task UnassignUser(ObjectId issueId, ObjectId userId)
+        {
+            var issue = await _issueRepo.GetAsync(issueId);
+            issue.AssignedUserIds.Remove(userId);
+            await _issueRepo.UpdateAsync(issue);
+        }
+    }
+}
