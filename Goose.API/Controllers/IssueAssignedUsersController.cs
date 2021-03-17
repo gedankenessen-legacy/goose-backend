b7dbe6a35868650b356cadf21066c5db21e44ad5
Yochyo <herodragon25@gmail.com>
2021-03-17 21:21:51 +0100

    AssignedUserController entspricht nun dem Res-Diagramm

--- Goose.API/Controllers/IssueAssignedUsersController.cs
+++ Goose.API/Controllers/IssueAssignedUsersController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Goose.API.Services;
+using Goose.Domain.Models.identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Goose.API.Controllers
@@ -16,17 +18,30 @@ namespace Goose.API.Controllers
             _issueService = issueService;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IList<User>>> GetAll([FromRoute] string issueId)
+        {
+            return Ok(await _issueService.GetAllOfIssueAsync(issueId.ToObjectId()));
+        }
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<IList<User>>> GetAll([FromRoute] string issueId, [FromRoute] string userId)
+        {
+            var user = _issueService.GetAssignedUserOfIssueAsync(issueId.ToObjectId(), userId: userId.ToObjectId());
+            if (user == null) return NotFound();
+            return Ok(await user);
+        }
+
         [HttpPut("{id}")]
         public async Task<ActionResult> Put([FromRoute] string issueId, [FromRoute] string id)
         {
-            await _issueService.AssignUser(issueId.ToObjectId(), id.ToObjectId());
+            await _issueService.AssignUserAsync(issueId.ToObjectId(), id.ToObjectId());
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete([FromRoute] string issueId, [FromRoute] string id)
         {
-            await _issueService.UnassignUser(issueId.ToObjectId(), id.ToObjectId());
+            await _issueService.UnassignUserAsync(issueId.ToObjectId(), id.ToObjectId());
             return NoContent();
         }
     }
--- Goose.API/Services/IssueAssignedUserService.cs
+++ Goose.API/Services/IssueAssignedUserService.cs
@@ -1,25 +1,47 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Goose.API.Repositories;
+using Goose.Domain.Models.identity;
+using Microsoft.AspNetCore.Identity;
 using MongoDB.Bson;
 
 namespace Goose.API.Services
 {
     public interface IIssueAssignedUserService
     {
-        Task AssignUser(ObjectId issueId, ObjectId userId);
-        Task UnassignUser(ObjectId issueId, ObjectId userId);
+        public Task<IList<User>> GetAllOfIssueAsync(ObjectId issueId);
+        public Task<User>? GetAssignedUserOfIssueAsync(ObjectId issueId, ObjectId userId);
+        Task AssignUserAsync(ObjectId issueId, ObjectId userId);
+        Task UnassignUserAsync(ObjectId issueId, ObjectId userId);
     }
 
-    public class IssueAssignedUserService: IIssueAssignedUserService
+    public class IssueAssignedUserService : IIssueAssignedUserService
     {
         private readonly IIssueRepository _issueRepo;
+        private readonly IUserRepository _userRepo;
 
-        public IssueAssignedUserService(IIssueRepository issueRepo)
+        public IssueAssignedUserService(IIssueRepository issueRepo, IUserRepository userRepo)
         {
             _issueRepo = issueRepo;
+            _userRepo = userRepo;
         }
 
-        public async Task AssignUser(ObjectId issueId, ObjectId userId)
+        public async Task<IList<User>> GetAllOfIssueAsync(ObjectId issueId)
+        {
+            var issue = await _issueRepo.GetAsync(issueId);
+            var users = issue.AssignedUserIds.Select(it => _userRepo.GetAsync(it));
+            return await Task.WhenAll(users);
+        }
+
+        public async Task<User>? GetAssignedUserOfIssueAsync(ObjectId issueId, ObjectId userId)
+        {
+            var issue = await _issueRepo.GetAsync(issueId);
+            if (!issue.AssignedUserIds.Contains(userId)) return null;
+            return await _userRepo.GetAsync(userId);
+        }
+
+        public async Task AssignUserAsync(ObjectId issueId, ObjectId userId)
         {
             var issue = await _issueRepo.GetAsync(issueId);
             if (!issue.AssignedUserIds.Contains(userId))
@@ -27,7 +49,7 @@ namespace Goose.API.Services
             await _issueRepo.UpdateAsync(issue);
         }
 
-        public async Task UnassignUser(ObjectId issueId, ObjectId userId)
+        public async Task UnassignUserAsync(ObjectId issueId, ObjectId userId)
         {
             var issue = await _issueRepo.GetAsync(issueId);
             issue.AssignedUserIds.Remove(userId);
