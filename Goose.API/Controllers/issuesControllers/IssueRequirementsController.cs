4e23a7414016be25329e4cf8619f956a4ca4a479
Yochyo <herodragon25@gmail.com>
2021-03-20 15:50:51 +0100

    corrected wrong route for IssueRequirementsController.cs

--- Goose.API/Controllers/issuesControllers/IssueRequirementsController.cs
+++ Goose.API/Controllers/issuesControllers/IssueRequirementsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Goose.API.Controllers.issuesControllers
 {
-    [Route("/api/issues/")]
+    [Route("/api/issues/{issueId}/requirements/")]
     [ApiController]
     public class IssueRequirementsController : Controller
     {
