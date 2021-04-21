1e283fa8377b03a087e5851de587f5ff91e5c54b
Philipp Heim <philipp-heim@outlook.de>
2021-04-21 10:23:41 +0200

    # base role req. + project role req + handler

---
+++ Goose.API/Authorization/Handlers/ProjectRoleHandler.cs
@@ -0,0 +1,18 @@
+﻿using Goose.API.Authorization.Requirements;
+using Goose.Domain.Models.Projects;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goose.API.Authorization.Handlers
+{
+    public class ProjectRoleHandler : AuthorizationHandler<ProjectRoleRequirement, Project>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ProjectRoleRequirement requirement, Project resource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
