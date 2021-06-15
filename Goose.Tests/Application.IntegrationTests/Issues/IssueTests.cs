9ba9cf6209b49454aae02349f8c880fd2b59daf0
Yochyo <herodragon25@gmail.com>
2021-06-15 20:08:04 +0200

    Unterticket nicht in "Wartend" wenn mit Startdatum als "Fehler" gestartet wird #127

---
+++ Goose.Tests/Application.IntegrationTests/Issues/IssueTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Goose.Domain.DTOs.Issues;
+using Goose.Domain.Models.Issues;
+using Goose.Domain.Models.Projects;
+using NUnit.Framework;
+
+namespace Goose.Tests.Application.IntegrationTests.issues
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class IssueTests
+    {
+        private class SimpleTestHelperBuilderStartDate : SimpleTestHelperBuilder
+        {
+            public override IssueDTO GetIssueDTOCopy(HttpClient client, SimpleTestHelper helper)
+            {
+                IssueDTO issueCopy = base.GetIssueDTOCopy(client, helper);
+                issueCopy.IssueDetail.RequirementsNeeded = false;
+                issueCopy.IssueDetail.StartDate = DateTime.Now.AddSeconds(300);
+                return issueCopy;
+            }
+        }
+        
+        [Test]
+        public async Task BugIssueStartsInWaitingStateIfStartDateNotReached()
+        {
+            var helper = await new SimpleTestHelperBuilderStartDate().Build();
+            Assert.AreEqual(State.WaitingState, helper.Issue.State.Name);
+        }
+    }
+}
